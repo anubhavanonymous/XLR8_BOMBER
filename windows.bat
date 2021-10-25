f6405e476b192a3ff053f3ba971484b071dd628b
badhackerjj <93131411+badhackerjj@users.noreply.github.com>
2021-10-25 17:11:19 +0530

    Create windows.bat

---
+++ windows.bat
@@ -0,0 +1,13 @@
+url for custom msg 
+http://cstm.herokuapp.com/bomb/number/mmsg/iknowupro
+bomb 1 srvr
+http://xlr801.herokuapp.com/bomb/number
+
+srvr 2 
+http://xlr802.herokuapp.com/bomb/number
+srvr3 url 
+http://xlr803.herokuapp.com/bomb/number
+srvr 4 url 
+http://xlr804.herokuapp.com/bomb/number
+srvr5 url
+http://xlr805.herokuapp.com/bomb/number
